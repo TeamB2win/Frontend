aabfed054e6340d72018236027d9e76dd2ea473b
moongni <mooooongni@gmail.com>
2023-07-21 18:00:53 +0900

    Feat 신고방법 페이지 추가신고방법에 관한 페이지 추가차후 이미지 변경 예정

--- wanted/src/pages/user/report/reportPage.js
+++ wanted/src/pages/user/report/reportPage.js
@@ -1,16 +1,186 @@
-import React from "react";
-import { Col, Container, Row, Card } from "react-bootstrap";
+import React, { useState } from "react";
+import { Col, Container, Row, Card, Nav, Stack, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 
 export default function ReportPage() {
+  // 접속한 기기가 모바일인지 확인
+  const isMobile = () => {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  };
+
+  var mobile = isMobile();
+
+  const reportList = [
+    {
+      imgUrl: "/images/report/report112.png",
+      title: "국번없이 112",
+      link: mobile ?
+        "tel://+01020802123" :
+        "",
+      target: mobile ?
+        "_blank" :
+        "",
+    },
+    {
+      imgUrl: "/images/report/report_smart_web.png",
+      title: "스마트 국민제보 홈페이지",
+      link: "http://onetouch.police.go.kr/",
+      target: "_black"
+    },
+    {
+      imgUrl: "/images/report/report_smart_app.png",
+      title: "스마트 국민제보 앱",
+      link: mobile ?
+        "https://play.google.com/store/apps/details?id=kr.go.police.cyber112&hl=ko&gl=US&pli=1" :
+        "",
+      target: mobile ?
+        "_blank" :
+        ""
+    }
+  ]
+
   const styles = {
     div: {
       "width": "80vw",
-      "margin": "30px auto",
+      "margin": "30px auto 0 auto",
       "padding": "2em 0",
       "color": "white"
     },
+    row: { "margin": "1em 0 0 0" },
+    title: {
+      "fontSize": "1.5em",
+      "fontWeight": "600",
+      "backgroundColor": "#2D55C9",
+      "padding": "0.5em 1em"
+    },
+    titleContent: {
+      "fontSize": "1.5em",
+      "fontWeight": "600",
+    },
+    subTitleNum: {
+      "fontSize": "1.5em",
+      "fontWeight": "600",
+      "color": "#FD6F22",
+      "padding": "0.5em 1em"
+    },
+    subTitleContent: {
+      "fontSize": "1.5em",
+      "fontWeight": "500",
+    },
+    hr: {
+      background: '#2D55C9',
+      color: '#2D55C9',
+      borderColor: '#2D55C9',
+      height: '3px',
+    }
+  }
+
+  return (
+    <div style={styles.div}>
+      <Container>
+        <Row className="justify-content-md-center">
+          {reportList.map(info => ReportCard(Object.assign({}, info, {"mobile": mobile})))}
+        </Row>
+      </Container>
+      <Container>
+        <Stack direction="horizontal" gap={3} style={styles.row}>
+          <div style={styles.title}>
+            신고안내
+          </div>
+          <div style={styles.titleContent}>
+            스마트 국민제보 홈페이지
+          </div>
+        </Stack>
+        <Stack direction="horizontal" gap={3} style={styles.row}>
+          <div style={styles.subTitleNum}>
+            01
+          </div>
+          <div style={styles.subTitleContent}>
+            사건제보-공개수배 제보 등록화면으로 이동하기
+          </div>
+        </Stack>
+        <Row style={styles.row}>
+          <Col xs={6} md={4}>
+            <Image src="/images/report/report_tutorial_01.png" rounded style={{ "width": "100%" }} />
+          </Col>
+          <Col style={{ "flexGrow": "1" }}>
+            사건제보, 공개수배 메뉴에서 사건을 선택하면 이동하는 조회화면에서 아래의 제보하기
+            버튼을 클릭하여 제보 등록화면으로 이동합니다.
+          </Col>
+        </Row>
+        <hr style={styles.hr}
+        />
+        <Stack direction="horizontal" gap={3} style={styles.row}>
+          <div style={styles.subTitleNum}>
+            02
+          </div>
+          <div style={styles.subTitleContent}>
+            제보내용 입력
+          </div>
+        </Stack>
+        <Row style={styles.row}>
+          <Col xs={6} md={4}>
+            <Image src="/images/report/report_tutorial_01.png" rounded style={{ "width": "100%" }} />
+          </Col>
+          <Col style={{ "flexGrow": "1" }}>
+            <p>1. 익명여부를 선택합니다. (익명제보 시 제보 내용은 참고로만 활용되며 보상에서 제외됩니다.)</p>
+            <p>2. (익명인 경우) 비밀번호를 입력해주세요. 익명제보조회 시 필요합니다.</p>
+            <p>3. 사건이 발생한 일자와 시간을 입력합니다.</p>
+            <p>4. 사건발생장소 설정 버튼을 클릭하여 팝업창에서 사건이 발생한 장소를 선택합니다.</p>
+            <p>5. 목격한 사건에 대한 제보내용을 입력합니다.</p>
+            <p>6. 첨부파일로 등록할 동영상 또는 이미지를 선택합니다.</p>
+            <p>7. (사건제보 사건 또는 공개수배에 대한 제보하는 경우) 사건 조회 시 하단에 보여지는 제보 목록에서
+              제보내용을 보여줄 것인지 선택합니다.</p>
+            <p>8. 개인정보 수집 및 이용동의, 제보정보 공유 동의 내용을 읽어보시고 동의에 체크해주세요.</p>
+          </Col>
+        </Row>
+        <hr style={styles.hr}/>
+        <Stack direction="horizontal" gap={3} style={styles.row}>
+          <div style={styles.subTitleNum}>
+            03
+          </div>
+          <div style={styles.subTitleContent}>
+            제보 등록하기
+          </div>
+        </Stack>
+        <Row style={styles.row}>
+          <Col xs={6} md={4}>
+            <Image src="/images/report/report_tutorial_01.png" rounded style={{ "width": "100%" }} />
+          </Col>
+          <Col style={{ "flexGrow": "1" }}>
+            <p>제보등록 버튼을 클릭하면 제보가 등록됩니다.</p>
+            <p>익명제보 시 팝업창으로 제보번호와 입력한 비밀번호를 한번 더 알려줍니다.
+            (익명제보조회 시 필요하니 꼭 기억하시기 바랍니다.)</p>
+            <p>제보한 내용은 실명제보인 경우 마이페이지 > 나의 제보조회 메뉴에서
+            익명제보인 경우 익명제보조회 메뉴에서 제보번호와 비밀번호를 입력하면 조회할 수 있습니다.</p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  )
+}
+
+// 각 신고 방법으로 이동하는 카드
+function ReportCard(props) {
+  const [isHover, setHover] = useState(false);
+
+  function handleMouseEnter() {
+    setHover(true);
+  };
+
+  function handleMouseLeave() {
+    setHover(false);
+  };
+
+  // 모바일 환경에서만 지원하는 신고방법의 경우 사용자 알림
+  function handleMobile() {
+    if (!props.mobile) {
+      alert("모바일 환경에서만 지원하는 기능입니다.")
+    }
+  }
+
+  const styles = {
     col: {
       "margin": "0 20px",
       "width": '15rem',
@@ -39,46 +209,24 @@ export default function ReportPage() {
       "color": "white",
       "textAlign": "center",
       "fontSize": "1.4em",
+      "textDecoration": isHover ? "underline" : "none"
     }
   }
-  
-  const isMobile = () => {
-    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-  };
 
   return (
-    <div style={styles.div}>
-      <Container>
-        <Row className="justify-content-md-center">
-          <Col xs lg="2" style={styles.col}>
-            <Card style={styles.card}>
-              <Card.Img variant="top" src={'/images/report/report112.png'} style={styles.cardImg}/>
-              <Card.Body style={styles.cardBody}>
-                <Card.Title style={styles.cardTitle}>국번없이 112</Card.Title>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col xs lg="2" style={styles.col}>
-            <Link to={'http://onetouch.police.go.kr/'} target="_blank" style={{"textDecoration": "none"}}>
-              <Card style={styles.card}>
-                <Card.Img variant="top" src={'/images/report/report_smart_web.png'} style={styles.cardImg}/>
-                <Card.Body style={styles.cardBody}>
-                  <Card.Title style={styles.cardTitle}>스마트 국민제보 홈페이지</Card.Title>
-                </Card.Body>
-              </Card>
-            </Link>
-          </Col>
-          <Col xs lg="2" style={styles.col}>
-            <Card style={styles.card}>
-              <Card.Img variant="top" src={'/images/report/report_smart_app.png'} style={styles.cardImg}/>
-              <Card.Body style={styles.cardBody}>
-                <Card.Title style={styles.cardTitle}>스마트 국민제보 앱</Card.Title>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-
-    </div>
+    <Col xs lg="2" style={styles.col}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onClick={handleMobile}
+    >
+      <Link to={props.link} target={props.target} style={{ "textDecoration": "none" }} onClick={props.onClick}>
+        <Card style={styles.card}>
+          <Card.Img variant="top" src={props.imgUrl} style={styles.cardImg} />
+          <Card.Body style={styles.cardBody}>
+            <Card.Title style={styles.cardTitle}>{props.title}</Card.Title>
+          </Card.Body>
+        </Card>
+      </Link>
+    </Col>
   )
 }
