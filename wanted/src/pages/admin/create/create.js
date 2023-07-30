02ab967f630995f106388a2b24521b2c72b1944c
moongni <mooooongni@gmail.com>
2023-07-30 21:38:19 +0900

    Fix: create 페이지 image와 form 데이터 분리backend에 image와 데이터를 동시에 요청보내는 것에 오류 발견image 업로드 api와 데이터 api 분리response에 따른 행동 추가 작성 필요

--- wanted/src/pages/admin/create/create.js
+++ wanted/src/pages/admin/create/create.js
@@ -1,64 +1,74 @@
 import React, { useState, useRef } from 'react';
+
+import axios from "axios";
 import "./create.css";
 
 function Create() {
     const [userInfo, setUserInfo] = useState({
+        wanted_id: '',
+        wanted_type: '',
         name: '',
         email: '',
         age: '',
-        photo: null,
+        image: null,
     });
+    const [imageFile, setImageFile] = useState({
+        file: null,
+        image: null
+    })
 
     const photoInputRef = useRef();
     const additionalPhotoInputRef = useRef();
 
     const [additionalPhoto, setAdditionalPhoto] = useState(null);
 
+    const handleImageChange = (event) => {
+        const file = event.target.files[0];
+        const reader = new FileReader();
+        reader.onloadend = () => {
+            const img = new Image();
+            const maxWidth = 1000;
+            const maxHeight = 1000;
+            const maxSize = 1024 * 1024
+            img.onload = () => {
+                const isMaxsize = img.size > maxSize
+                // const isSquare = img.width === img.height;
+                if (isMaxsize) {
+                    const msg = '파일 용량을 1MB 이하로 제한해주세요.';
+                    window.alert(msg);
+                    handleDeletePhoto();
+                }
+                // if (isSquare) {
+                //     // Image has a square aspect ratio, do something
+                //     // console.log('정방형 이미지 입니다.');
+                //     if (img.width > maxWidth || img.height > maxHeight) {
+                //         window.alert("이미지 크기를 1000px 이하로 제한해주세요.");
+                //     } else {
+                //         setImageFile(reader.result)
+                //     }
+                // } else {
+                //     // Image does not have a square aspect ratio, do something else
+                //     // const msg = 'Image must have a "square aspect ratio". Please choose another image.';
+                //     // window.alert(msg);
+                //     // handleDeletePhoto();
+                // }
+                if (img.width > maxWidth || img.height > maxHeight) {
+                    window.alert("이미지 크기를 1000px 이하로 제한해주세요.");
+                } else {
+                    setImageFile({ file: file, image: reader.result })
+                }
+            };
+            img.src = reader.result;
+        };
+        reader.readAsDataURL(file);
+        console.log("이미지 등록")
+
+        event.target.style.display = 'none';
+    }
+
     const handleInputChange = (event) => {
         const { name, type } = event.target;
-        if (type === 'file') {
-            const file = event.target.files[0];
-            const reader = new FileReader();
-            // reader.onloadend = () => {
-            //     setUserInfo({ ...userInfo, [name]: reader.result });
-            // };
-            reader.onloadend = () => {
-                const img = new Image();
-                const maxWidth = 1000;
-                const maxHeight = 1000;
-                const maxSize = 1024 * 1024
-                img.onload = () => {
-                    const isMaxsize = img.size > maxSize
-                    const isSquare = img.width === img.height;
-                    if(isMaxsize){
-                        const msg = '파일 용량을 1MB 이하로 제한해주세요.';
-                            window.alert(msg);
-                            handleDeletePhoto();
-                    }
-                    if (isSquare) {
-                        // Image has a square aspect ratio, do something
-                        console.log('정방형 이미지 입니다.');
-                        if (img.width > maxWidth || img.height > maxHeight) {
-                            window.alert("이미지 크기를 1000px 이하로 제한해주세요.");
-                        } else{
-                            setUserInfo({ ...userInfo, [name]: reader.result })
-                        }
-                    } else {
-                        // Image does not have a square aspect ratio, do something else
-                        const msg = 'Image must have a "square aspect ratio". Please choose another image.';
-                        window.alert(msg);
-                        handleDeletePhoto();
-                    }
-                };
-                img.src = reader.result;
-            };
-            reader.readAsDataURL(file);
-            console.log("이미지 등록")
-
-            event.target.style.display = 'none';
-        } else {
-            setUserInfo({ ...userInfo, [name]: event.target.value });
-        }
+        setUserInfo({ ...userInfo, [name]: event.target.value });
     };
 
     const handleAdditionalPhotoChange = (event) => {
@@ -74,7 +84,7 @@ function Create() {
     };
 
     const handleDeletePhoto = () => {
-        setUserInfo({ ...userInfo, photo: null });
+        setImageFile({ file: null, image: null });
         photoInputRef.current.value = '';
         photoInputRef.current.style.display = 'inline';
     };
@@ -88,19 +98,74 @@ function Create() {
     const handleSubmit = (event) => {
         event.preventDefault();
         // 여기서 정보를 API로 전송하거나 다른 처리를 수행할 수 있습니다.
-        if (!userInfo.name || !userInfo.age || !userInfo.sex || !userInfo.wanted_type || !userInfo.criminal || !userInfo.registerd_address || !userInfo.residence) {
+        if (!imageFile.image) {
+            alert("이미지를 선택해주세요");
+            return;
+        }
+        if (!userInfo.name || !userInfo.age || !userInfo.sex || !userInfo.wanted_type || !userInfo.criminal || !userInfo.registered_address || !userInfo.residence) {
             alert("빈 칸을 모두 입력해주세요.");
             return;
         }
         handleConfirmation();
     };
 
-    const handleConfirmation = () => {
+    const handleConfirmation = async () => {
         const result = window.confirm("정보 등록을 완료하시겠습니까?");
         if (result) {
-            handleSubmit();
-        } else {
-            // 사용자가 취소를 누른 경우 아무 작업 없음
+            // uploading the image file
+            const formData = new FormData();
+            formData.append(
+                "file",
+                imageFile.file
+            )
+
+            const headers = { "Content-Type": `${imageFile.file.type}; charset=UTF-8` }
+            const response = await axios.post(
+                "http://localhost:8000/admin/uploadimage",
+                formData,
+                headers
+            )
+
+            if (response.status !== 201) {
+                alert(response.data);
+                return;                
+            }
+            
+            // Wanted 데이터 테이블에 등록
+            const image_path = response.data.imagePath
+            axios.post(
+                "http://localhost:8000/admin",
+                {
+                    ...userInfo,
+                    image: image_path
+                },
+            ).then(function (response) {
+                console.log(response.data);
+                // TODO data hash 값 redux에 저장
+
+                
+                alert("등록이 완료되었습니다.");
+                // 차후 domain 주소로 변경
+                // window.location.replace('http://localhost:3000/admin');
+            }).catch(function (error) {
+                if (error.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    console.log(error.response.headers);
+                    console.log(error.response.data);
+                    alert("에러가 발생하였습니다.", error.response.data)
+                } else if (error.request) {
+                    // The request was made but no response was received
+                    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                    // http.ClientRequest in node.js
+                    alert("잘못된 요청입니다. 잠시 후 다시 시도해주세요");
+                } else {
+                    // Something happened in setting up the request that triggered an Error
+                    console.log('Error', error.message);
+                    alert("잘못된 요청입니다. 잠시 후 다시 시도해주세요", error.message);
+                }
+                console.log(error.config);
+            });
         }
     };
 
@@ -109,10 +174,10 @@ function Create() {
             <h1 className="header">공개수배자 정보 등록</h1>
             <div className='photo-containers'>
                 <div className="photo-container">
-                    {userInfo.photo ? (
+                    {imageFile.image ? (
                         <>
                             <img
-                                src={userInfo.photo}
+                                src={imageFile.image}
                                 alt="User"
                                 style={{ maxWidth: '200px', maxHeight: '200px', marginBottom: "1rem" }}
                             />
@@ -130,9 +195,9 @@ function Create() {
                     <input style={{ maxWidth: '214px' }}
                         type="file"
                         accept="image/*"
-                        name="photo"
+                        name="image"
                         ref={photoInputRef}
-                        onChange={handleInputChange}
+                        onChange={handleImageChange}
                     />
                 </div>
             </div>
@@ -149,13 +214,18 @@ function Create() {
                 </div>
                 <div className="form-group">
                     <label>성별</label>
-                    <input
+                    <select name='sex' onChange={handleInputChange}>
+                        <option>성별</option>
+                        <option value={true}>남자</option>
+                        <option value={false}>여자</option>
+                    </select>
+                    {/* <input
                         type="bool"
                         name="sex"
                         value={userInfo.sex}
                         onChange={handleInputChange}
                         required
-                    />
+                    /> */}
                 </div>
                 <div className="form-group">
                     <label>나이</label>
@@ -169,14 +239,31 @@ function Create() {
                 </div>
                 <div className="form-group">
                     <label>유형</label>
-                    <input
+                    <select name='wanted_type' onChange={handleInputChange}>
+                        <option>유형</option>
+                        <option value={true}>종합</option>
+                        <option value={false}>긴급</option>
+                    </select>
+                    {/* <input
                         type="bool"
                         name="wanted_type"
                         value={userInfo.wanted_type}
                         onChange={handleInputChange}
                         required
-                    />
+                    /> */}
                 </div>
+                {userInfo.wanted_type === 'true' &&
+                    <div className="form-group">
+                        <label>공개수배번호</label>
+                        <input
+                            type="bool"
+                            name="wanted_id"
+                            value={userInfo.wanted_id}
+                            onChange={handleInputChange}
+                            required
+                        />
+                    </div>
+                }
                 <div className="form-group">
                     <label>죄명</label>
                     <input
@@ -191,7 +278,7 @@ function Create() {
                     <label>주민등록상 주소지</label>
                     <input
                         type="text"
-                        name="registerd_address"
+                        name="registered_address"
                         value={userInfo.registerd_address}
                         onChange={handleInputChange}
                         required
