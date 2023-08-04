ed38a32281c0e7c12ca5b98a00a85203e4aa2074
hwaxrang <hwaxang@gmail.com>
2023-08-04 16:08:47 +0900

    Fix: 버튼활성화 변수 초기화

--- wanted/src/pages/admin/update/update.js
+++ wanted/src/pages/admin/update/update.js
@@ -24,7 +24,7 @@ function Update() {
 
     const [additionalPhoto, setAdditionalPhoto] = useState(null);
 
-    const [isCheckboxChecked, setIsCheckboxChecked] = useState(false);
+    const [isButtonDisabled, setIsButtonDisabled] = useState(true);
 
     const columns = ["name", "sex", "wantedType", "age"];
     
